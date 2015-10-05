2de6e0f8aa2a9d87ac4bb769e5bd6a29f72889ca
Wolfgang Meyers <wolfgang@donuts.co>
2015-10-05 09:55:00 -0700

    Initial commit - adding rake.go

---
+++ rake/rake.go
@@ -0,0 +1,255 @@
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package rake
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+	"sort"
+	"io/ioutil"
+	"fmt"
+	"os"
+)
+
+var sentenceDelimiters *regexp.Regexp = regexp.MustCompile("[\\[\\]\n\\.!?,;:\t\\-\\\"\\(\\)\\'\u2019\u2013]")
+var wordSep *regexp.Regexp = regexp.MustCompile("\\s+")
+var wordSplitter *regexp.Regexp = regexp.MustCompile("[^a-zA-Z0-9_\\+\\-/]")
+
+func SplitSentences(text string) []string {
+	return sentenceDelimiters.Split(text, -1)
+}
+
+func IsAcceptable(phrase string, minCharLength int, maxWordsLength int) bool {
+	// a phrase must have a min length in characters
+	if len(phrase) < minCharLength {
+		return false
+	}
+
+	// a phrase must have a max number of words
+	words := wordSep.Split(phrase, -1)
+	if len(words) > maxWordsLength {
+		return false
+	}
+	digits := 0
+	alpha := 0
+	for _, c := range phrase {
+		if c > '0' && c < '9' {
+			digits++
+		} else if (c > 'a' && c < 'z') || (c > 'A' && c < 'Z') {
+			alpha++
+		}
+	}
+	// a phrase must have at least one alpha character
+	if alpha == 0 {
+		return false
+	}
+	// a phrase must have more alpha than digits characters
+	if digits > alpha {
+		return false
+	}
+	return true
+}
+
+func GenerateCandidateKeywords(sentenceList []string, stopwordPattern *regexp.Regexp, minCharLength int, maxWordsLength int) []string {
+	phraseList := []string{}
+	for _, s := range sentenceList {
+		tmp := stopwordPattern.ReplaceAllString(strings.TrimSpace(s), "|")
+		phrases := strings.Split(tmp, "|")
+		for _, phrase := range phrases {
+			phrase = strings.ToLower(strings.TrimSpace(phrase))
+			if phrase != "" && IsAcceptable(phrase, minCharLength, maxWordsLength) {
+				phraseList = append(phraseList, phrase)
+			}
+		}
+	}
+	return phraseList
+}
+
+func IsNumber(s string) bool {
+	if strings.Index(s, ".") != -1 {
+		_, err := strconv.ParseFloat(s, 32)
+		return err == nil
+	} else {
+		_, err := strconv.ParseInt(s, 10, 32)
+		return err == nil
+	}
+}
+
+//def is_number(s):
+//    try:
+//        float(s) if '.' in s else int(s)
+//        return True
+//    except ValueError:
+//        return False
+
+func SeparateWords(text string, minWordReturnSize int) []string {
+	//    Utility function to return a list of all words that are have a length
+	//    greater than a specified number of characters.
+	//    @param text The text that must be split in to words.
+	//    @param min_word_return_size The minimum no of characters
+	//           a word must have to be included.
+	words := []string{}
+	for _, singleWord := range wordSplitter.Split(text, -1) {
+		currentWord := strings.ToLower(strings.TrimSpace(singleWord))
+		// leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
+		if len(currentWord) > minWordReturnSize && currentWord != "" && !IsNumber(currentWord) {
+			words = append(words, currentWord)
+		}
+	}
+	return words
+}
+
+func CalculateWordScores(phraseList []string) map[string]float64 {
+	wordFrequency := map[string]int{}
+	wordDegree := map[string]int{}
+	for _, phrase := range phraseList {
+		wordList := SeparateWords(phrase, 0)
+		wordListLength := len(wordList)
+		wordListDegree := wordListLength - 1
+		// if word_list_degree > 3: word_list_degree = 3 #exp.
+		for _, word := range wordList {
+			wordFrequency[word]++
+			wordDegree[word] += wordListDegree
+		}
+	}
+	for item := range wordFrequency {
+		wordDegree[item] = wordDegree[item] + wordFrequency[item]
+	}
+	// Calculate Word scores = deg(w)/frew(w)
+	wordScore := map[string]float64{}
+	for item := range wordFrequency {
+		wordScore[item] = float64(wordDegree[item]) / float64(wordFrequency[item])
+	}
+	return wordScore
+}
+
+func StringCount(stringList []string) map[string]int {
+	stringCount := map[string]int{}
+	for _, item := range stringList {
+		stringCount[item]++
+	}
+	return stringCount
+}
+
+func GenerateCandidateKeywordScores(phraseList []string, wordScore map[string]float64, minKeywordFrequency int) map[string]float64 {
+	keywordCandidates := map[string]float64{}
+	phraseCounts := StringCount(phraseList)
+	for _, phrase := range phraseList {
+		if minKeywordFrequency > 1 {
+			if phraseCounts[phrase] < minKeywordFrequency {
+				continue
+			}
+		}
+		wordList := SeparateWords(phrase, 0)
+		var candiateScore float64 = 0
+		for _, word := range wordList {
+			candiateScore += wordScore[word]
+		}
+		keywordCandidates[phrase] = candiateScore
+	}
+	
+	return keywordCandidates
+}
+
+func MapToKeywordScores(keywordScoreMap map[string]float64) []KeywordScore {
+	keywordScores := []KeywordScore{}
+	for key, value := range keywordScoreMap {
+		keywordScore := KeywordScore{
+			Keyword: key,
+			Score: value,
+		}
+		keywordScores = append(keywordScores, keywordScore)
+	}
+	return keywordScores
+}
+
+func loadStopWords(stopWordFile string) []string {
+	stopWords := []string{}
+	contents, err := ioutil.ReadFile(stopWordFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range(lines) {
+		line = strings.ToLower(strings.TrimSpace(line))
+		if strings.Index(line, "#") != 0 {
+			// in case more than one per line
+			for _, word := range strings.Split(line, " ") {
+				stopWords = append(stopWords, word)
+			}
+			
+		}
+	}
+	return stopWords
+}
+
+func BuildStopWordRegex(stopWordFilePath string) *regexp.Regexp {
+	stopWordList := loadStopWords(stopWordFilePath)
+	stopWordRegexList := []string{}
+	for _, word := range stopWordList {
+		wordRegex := "\\b" + word + "\\b"
+		stopWordRegexList = append(stopWordRegexList, wordRegex)
+	}
+	stopWordPattern := regexp.MustCompile(strings.Join(stopWordRegexList, "|"))
+	return stopWordPattern
+}
+
+// ByScore implements sort.Interface for []KeywordScore based on
+// the Score field. Sorts in reverse order.
+type ByScore []KeywordScore
+
+func (a ByScore) Len() int           { return len(a) }
+func (a ByScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByScore) Less(i, j int) bool { return a[i].Score > a[j].Score }
+
+type Rake struct {
+	stopWordsPath       string
+	stopWordsPattern    *regexp.Regexp
+	minCharLength       int
+	maxWordsLength      int
+	minKeywordFrequency int
+}
+
+type KeywordScore struct {
+	Keyword string
+	Score   float64
+}
+
+func NewRake(stopWordsPath string, minCharLength int, maxWordsLength int, minKeywordFrequency int) *Rake {
+	return &Rake{
+		stopWordsPath: stopWordsPath,
+		stopWordsPattern: BuildStopWordRegex(stopWordsPath),
+		minCharLength: minCharLength,
+		maxWordsLength: maxWordsLength,
+		minKeywordFrequency: minKeywordFrequency,
+	}
+}
+
+func (rake *Rake) Run(text string) []KeywordScore {
+	sentenceList := SplitSentences(text)
+	phraseList := GenerateCandidateKeywords(sentenceList, rake.stopWordsPattern, rake.minCharLength, rake.maxWordsLength)
+	wordScores := CalculateWordScores(phraseList)
+	keywordCandidates := GenerateCandidateKeywordScores(phraseList, wordScores, rake.minKeywordFrequency)
+	keywordScores := MapToKeywordScores(keywordCandidates)
+	sort.Sort(ByScore(keywordScores))
+	return keywordScores
+}
